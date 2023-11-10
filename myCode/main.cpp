3d7e3594fc7516624a445978dd7df4dc1b3b61a9
stmacebal <mateo.ceballosquerol@stud.h-da.de>
2023-11-10 19:31:31 +0100

    added preprocessor function for counter name

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -1,6 +1,13 @@
-
 #include <iostream>
 #include "CModuloNCounter.h"
+
+
+#define COUNTER_NAME(x) \
+    ((x) == 2 ? "binary" : \
+    ((x) == 8 ? "octal" : \
+    ((x) == 10 ? "decimal" : \
+    ((x) == 16 ? "hexadecimal" : "N/A"))))
+
 using namespace std;
 
 int main (void)
@@ -9,25 +16,30 @@ int main (void)
 	unsigned int base;
 	unsigned int digits;
 	unsigned int maxCount;
+
 	do
 	{
-		cout << "Enter the base and the number of digits you want for your counter and the max count: " << endl;
-		cin >> base;
+		cout << "Please enter the parameters of your counter:" << endl;
+		cout << "How many digits?:" << endl;
 		cin >> digits;
+		cout << "What base? (2/8/10/16):" << endl;
+		cin >> base;
+		cout << "What is the maximum count?" << endl;
 		cin >> maxCount;
 
 	}while(!(digits > 1) && !(base == 2 || base == 8 || base == 10 || base == 16));
 
 
-	cout << "MODULO COUNTER : base " << base << " counter with " << digits << " digits and a max count of " << maxCount << endl;
-	CModuloNCounter mdc(digits,base);
+	CModuloNCounter Counter(base, digits);
+	cout << digits << " digit " << COUNTER_NAME(base) << " counter" << endl;
+
 	for(unsigned int count = 0; count < maxCount; count++)
 	{
-		mdc.increment();
-		mdc.print();
-		cout << endl;
+        //++Counter;
+        //Counter.print();
+        Counter++;
+        Counter.print();
 	}
-
 
 	return 0;
 }
